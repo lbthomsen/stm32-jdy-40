e6b5c85ce218c99131ed342124223d416e61e386
Lars Boegild Thomsen <lth@netcompartner.com>
2022-04-02 08:09:55 +0800

    Hmmm

--- jdy40.c
+++ jdy40.c
@@ -17,7 +17,7 @@
  *
  */
 
-//char buf[128] = ""; //
+char buf[128];
 
 /*
  * Internal functions
@@ -39,9 +39,25 @@ inline void jdy40_atoff(JDY40_HandleTypeDef *jdy40) {
 	HAL_GPIO_WritePin(jdy40->set_port, jdy40->set_pin, GPIO_PIN_SET);
 }
 
-JDY40_result_t jdy40_send_receive(JDY40_HandleTypeDef *jdy40, char *buf) {
+JDY40_result_t jdy40_send_receive(JDY40_HandleTypeDef *jdy40, char *cmd) {
+
+	sprintf(&buf, "AT%s\r\n", cmd);
+
 	JDY40_DBG("Sending command: %s", buf);
-	HAL_UART_Transmit(jdy40->uartHandle, strcat("AT", buf, "\r\n"), strlen(buf) + 4, 100);
+
+	HAL_UART_Transmit(jdy40->uartHandle, buf, strlen(buf) + 4, 100);
+
+	sprintf(&buf, "");
+	char ch;
+
+	while (ch != 10) {
+		HAL_UART_Receive(jdy40->uartHandle, &ch, 1, 10);
+		sprintf(&buf, "%s%c", buf, ch);
+	}
+
+	JDY40_DBG("Received: %s", buf);
+
+	return JDY40_Ok;
 }
 
 JDY40_result_t jdy40_send_command(JDY40_HandleTypeDef *jdy40) {
@@ -78,7 +94,11 @@ JDY40_result_t jdy40_init(JDY40_HandleTypeDef *jdy40, UART_HandleTypeDef *uart, 
 	jdy40_on(jdy40);
 	HAL_Delay(1);
 
-	buf[0] = 0;
+	jdy40_aton(jdy40);
+
+	jdy40_send_receive(jdy40, "+BAUD");
+
+	//buf[0] = 0;
 
 	// Let us register the RX callback
 	HAL_UART_RegisterCallback(uart, HAL_UART_RX_COMPLETE_CB_ID, uart_rx_cb);
